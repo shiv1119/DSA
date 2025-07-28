d5e424880a085ca104813f2392ce692520ed8504
shiv1119 <shivnandanverma63@gmail.com>
2025-07-28 11:53:58 +0530

    longest common substring printing done

--- DP/LongestCommonSubsequences/largest_common_substring.py
+++ DP/LongestCommonSubsequences/largest_common_substring.py
@@ -16,4 +16,21 @@ class Solution:
         
         return max_len
         
+#print
+class Solution:
+    def longestCommonSubstr(self, s1, s2):
+        # code here
+        m = len(s1)
+        n = len(s2)
+        end_pos = 0
+        dp = [[0] * (n+1) for _ in range(m+1)]
+        max_len = 0
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if (s1[i-1] == s2[j-1]):
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                    if max_len < dp[i][j]:
+                        max_len = dp[i][j]
+                        end_pos = i
         
+        return s1[end_pos-max_len:end_pos]  
