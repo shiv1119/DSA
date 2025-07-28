7ff70487eb3e0972b40669cb546a40d359c8dc20
shiv1119 <shivnandanverma63@gmail.com>
2025-07-28 11:46:27 +0530

    longest common substring done

---
+++ DP/LongestCommonSubsequences/largest_common_substring.py
@@ -0,0 +1,20 @@
+#User function Template for python3
+
+#Returns length
+class Solution:
+    def longestCommonSubstr(self, s1, s2):
+        # code here
+        m = len(s1)
+        n = len(s2)
+        dp = [[0] * (n+1) for _ in range(m+1)]
+        max_len = 0
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if (s1[i-1] == s2[j-1]):
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                    max_len = max(max_len, dp[i][j])
+        
+
+        return max_len
+        
+        
